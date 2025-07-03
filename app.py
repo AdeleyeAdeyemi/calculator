0cc671cdcccc0b2d3590b42d89f52d12e2ef4078
Undefinedboss <josekadel355@gmail.com>
2025-07-03 11:23:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,7 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8777, debug=True)
 
