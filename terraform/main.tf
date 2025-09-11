319391cc5f3af28805bf1a80b5f1282bde0c19f7
Adeleye_Adeyemi <josekadel355@gmail.com>
2025-09-11 14:40:38 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,55 +1,36 @@
 provider "aws" {
-  region     = var.region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  region = var.region
+
 }
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["amazon"]
-}
-
-# Generate SSH keypair
-resource "tls_private_key" "my_key" {
+# Generate new key pair each run
+resource "tls_private_key" "generated" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-# Upload public key to AWS
-resource "aws_key_pair" "generated_key" {
-  key_name   = "terraform-generated-key"
-  public_key = tls_private_key.my_key.public_key_openssh
+resource "aws_key_pair" "generated" {
+  key_name   = "jenkins-key-${formatdate("YYYYMMDDHHmmss", timestamp())}"
+  public_key = tls_private_key.generated.public_key_openssh
 }
 
-# Write private key to disk
-resource "local_file" "private_key_pem" {
-  content          = tls_private_key.my_key.private_key_pem
-  filename         = "${path.module}/../ansible/terraform-key.pem"
-  file_permission  = "0400"
+resource "local_file" "private_key" {
+  content         = tls_private_key.generated.private_key_pem
+  filename        = "${path.module}/jenkins-key.pem"
+  file_permission = "0400"
 }
 
-# Create Security Group with SSH access
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH"
-  vpc_id      = var.vpc_id
+# Security group
+resource "aws_security_group" "ec2_sg" {
+  name_prefix = "ec2-sg"
+  description = "Allow SSH and HTTP"
+
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Consider restricting this for production
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -58,25 +39,74 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
 
-# Launch EC2 instance
-resource "aws_instance" "flask_app" {
-  ami                         = data.aws_ami.amazon_linux.id
-  instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.generated_key.key_name
-  associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+  ingress {
+    from_port   = 8777
+    to_port     = 8777
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5044
+    to_port     = 5044
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9200
+    to_port     = 9200
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9600
+    to_port     = 9600
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
-    Name = "flask-e-commerce-app"
-  }
-
-  provisioner "local-exec" {
-    command = <<EOT
-echo "[web]
-${self.public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=terraform-key.pem" > ../ansible/inventory.ini
-EOT
+    Name = "flask-app-sg"
   }
 }
+
+##############################
+# AMAZON LINUX AMI
+##############################
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+##############################
+# EC2 INSTANCE
+##############################
+resource "aws_instance" "ec2" {
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = "t3.micro"
+  key_name                    = aws_key_pair.generated.key_name
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "jenkins_ec2"
+  }
+}
+
+
 
